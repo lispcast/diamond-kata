0ea73f123ee89a52db8de1ecfdf3bc3a7e2aba9f
Eric Normand <ericwnormand@gmail.com>
2016-02-06 06:14:20 -0600

    Change project license information.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject diamond-kata "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
