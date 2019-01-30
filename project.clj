30ecbe3cb6cd32ec7c29dff8a1bb27fddb654436
Eric Normand <ericwnormand@gmail.com>
2019-01-30 16:20:57 -0600

    Update Clojure to 1.10

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]])
